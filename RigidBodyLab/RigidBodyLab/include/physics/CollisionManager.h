35246711075801b2c0af4137ea2062303a207939
beongkyuPark <byeonggyu.park.kr@gmail.com>
2023-12-17 08:58:13 -0800

    adding collision detection logics

--- RigidBodyLab/RigidBodyLab/include/physics/CollisionManager.h
+++ RigidBodyLab/RigidBodyLab/include/physics/CollisionManager.h
@@ -18,6 +18,48 @@ namespace Physics {
         float m_penetrationTolerance;
         float m_closingSpeedTolerance;
 
+        float CalcPenetration(const BoxCollider* box1, const BoxCollider* box2,
+            const std::shared_ptr<RigidBody>& body1, const std::shared_ptr<RigidBody>& body2,
+            const Vector3& axis) {
+
+            if (!box1 || !box2) {
+                // Handle the error: one of the objects does not have a BoxCollider
+                return 0.0f;
+            }
+
+            // Default values for position and orientation in case of static objects
+            Vector3 position1 = body1 ? body1->GetPosition() : Vector3(0, 0, 0);
+            Vector3 position2 = body2 ? body2->GetPosition() : Vector3(0, 0, 0);
+
+            Vec3 extents1 = std::get<Vec3>(box1->GetScale()); // Assuming GetScale() returns a std::variant<Vec3, float>
+            Vec3 extents2 = std::get<Vec3>(box2->GetScale()); // Assuming GetScale() returns a std::variant<Vec3, float>
+
+            Vector3 centerToCenter = position2 - position1;
+            float projectedCenterToCenter = abs(centerToCenter.Dot(axis));
+
+            // Use default axes (i.e., world axes) if no RigidBody is present
+            Vector3 axes1[3] = { Vector3(1, 0, 0), Vector3(0, 1, 0), Vector3(0, 0, 1) };
+            Vector3 axes2[3] = { Vector3(1, 0, 0), Vector3(0, 1, 0), Vector3(0, 0, 1) };
+
+            if (body1) {
+                for (int i = 0; i < 3; ++i) axes1[i] = body1->GetAxis(i);
+            }
+            if (body2) {
+                for (int i = 0; i < 3; ++i) axes2[i] = body2->GetAxis(i);
+            }
+
+            float projectedSum =
+                abs((axes1[0] * extents1.x).Dot(axis)) +
+                abs((axes1[1] * extents1.y).Dot(axis)) +
+                abs((axes1[2] * extents1.z).Dot(axis)) +
+
+                abs((axes2[0] * extents2.x).Dot(axis)) +
+                abs((axes2[1] * extents2.y).Dot(axis)) +
+                abs((axes2[2] * extents2.z).Dot(axis));
+
+            return projectedSum - projectedCenterToCenter;
+        }
+
         // Function to handle Sphere-Box collision
         std::optional<CollisionData> FindCollisionFeaturesSphereBox(const SphereCollider* sphere, const BoxCollider* box,
             const std::shared_ptr<RigidBody>& body1, const std::shared_ptr<RigidBody>& body2) {
@@ -55,7 +97,7 @@ namespace Physics {
             collisionData.penetrationDepth = radius - std::sqrt(distanceSquared);
             collisionData.restitution = m_objectRestitution;
             collisionData.friction = m_friction;
-			return collisionData;
+            return collisionData;
         }
 
         // Function to handle Sphere-Sphere collision
@@ -66,26 +108,93 @@ namespace Physics {
         }
 
         // Function to handle Box-Box collision
-        std::optional<CollisionData> FindCollisionFeaturesBoxBox(const BoxCollider* box1, const BoxCollider* box2,
+        std::optional<CollisionData> FindCollisionFeaturesBoxBox(
+            const BoxCollider* box1, const BoxCollider* box2,
             const std::shared_ptr<RigidBody>& body1, const std::shared_ptr<RigidBody>& body2) {
-            // Implement Box-Box collision detection logic here
-            return std::nullopt;
+
+            // Broad phase check using sphere bounding (optional for efficiency)
+            Vec3 extents1 = std::get<Vec3>(box1->GetScale());//std::variant<Vec3, float>
+            Vec3 extents2 = std::get<Vec3>(box2->GetScale());//std::variant<Vec3, float>
+
+            float radius1 = extents1.x > extents1.y ? (extents1.x > extents1.z ? extents1.x : extents1.z) : (extents1.y > extents1.z ? extents1.y : extents1.z);
+            float radius2 = extents2.x > extents2.y ? (extents2.x > extents2.z ? extents2.x : extents2.z) : (extents2.y > extents2.z ? extents2.y : extents2.z);
+            Vector3 distanceVec = body2->GetPosition() - body1->GetPosition();
+            if (distanceVec.LengthSquared() > (radius1 + radius2) * (radius1 + radius2)) {
+                return std::nullopt;
+            }
+
+            // Axes to test for potential separating planes
+            std::vector<Vector3> axes;
+
+            // Axes of the first box
+            for (int i = 0; i < 3; ++i) {
+                axes.push_back(body1->GetAxis(i));
+            }
+
+            // Axes of the second box
+            for (int i = 0; i < 3; ++i) {
+                axes.push_back(body2->GetAxis(i));
+            }
+
+            // Edge-edge axes (cross products)
+            for (int i = 0; i < 3; ++i) {
+                for (int j = 0; j < 3; ++j) {
+                    Vector3 cross = axes[i].Cross(axes[3 + j]);
+                    if (cross.LengthSquared() > 0.001f) { // To avoid near-zero length vectors
+                        axes.push_back(cross.Normalize());
+                    }
+                }
+            }
+
+            float minPenetration = FLT_MAX;
+            Vector3 minAxis;
+            for (const auto& axis : axes) {
+                float penetration = CalcPenetration(box1, box2, body1, body2, axis);
+                if (penetration <= 0) {
+                    return std::nullopt; // Separating axis found, no collision
+                }
+                if (penetration < minPenetration) {
+                    minPenetration = penetration;
+                    minAxis = axis;
+                }
+            }
+
+            // Collision normal should point from body2 to body1
+            Vector3 collisionNormal = minAxis;
+            if (collisionNormal.Dot(body2->GetPosition() - body1->GetPosition()) < 0) {
+                collisionNormal = -collisionNormal;
+            }
+
+            // Determine contact points and other collision properties
+            CollisionData collisionData;
+            collisionData.bodies[0] = body1;
+            collisionData.bodies[1] = body2;
+            collisionData.collisionNormal = collisionNormal;
+            collisionData.penetrationDepth = minPenetration;
+            // Set restitution and friction based on material properties of bodies
+            //collisionData.restitution = CalculateRestitution(body1, body2);
+            //collisionData.friction = CalculateFriction(body1, body2);
+            collisionData.restitution = m_objectRestitution;
+            collisionData.friction = m_friction;
+
+            // Calculate contact points...
+            // (Implement CalcBoxBoxContactPoints or similar function to determine contact points)
+
+            return collisionData;
         }
 
         // Function to handle Sphere-Plane collision
         std::optional<CollisionData> FindCollisionFeaturesSpherePlane(const SphereCollider* sphere, const PlaneCollider* plane,
             const std::shared_ptr<RigidBody>& body1) {
-            // Implement Sphere-Sphere collision detection logic here
+            // Implement Sphere-Plane collision detection logic here
             return std::nullopt;
         }
-
         // Function to handle Box-Plane collision
         std::optional<CollisionData> FindCollisionFeaturesSpherePlane(const BoxCollider* box, const PlaneCollider* plane,
             const std::shared_ptr<RigidBody>& body1) {
-            // Implement Sphere-Sphere collision detection logic here
+            // Implement Box-Plane collision detection logic here
             return std::nullopt;
         }
-
     public:
         CollisionManager()
             : m_friction(0.6f), m_objectRestitution(0.5f), m_groundRestitution(0.2f),
@@ -136,12 +245,6 @@ namespace Physics {
             }
 
             return std::nullopt; // No collision detected
-        }
-        void ResolveCollisions() {
-            for (auto& collision : m_collisions) {
-                // Resolve each collision
-                // Example: adjust positions, apply impulses, etc.
-            }
         }
     };
 }
